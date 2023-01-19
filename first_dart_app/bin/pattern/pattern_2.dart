a1983fd1f94b571527d6bc0cd6dc1fa9c0af25d5
Rohit <rohitsharma.rs4518@gmail.com>
2023-01-19 18:55:41 +0530

    pattern program

--- first_dart_app/bin/pattern/pattern_2.dart
+++ first_dart_app/bin/pattern/pattern_2.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> args) {
   // WAP to print the following pattern
   /*
@@ -7,4 +9,10 @@ void main(List<String> args) {
   1234
   12345
   */
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write(j);
+    }
+    stdout.write("\n");
+  }
 }
