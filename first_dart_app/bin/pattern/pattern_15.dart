f6e648b7e6fdffddbf1dfdeb05984da6d11431e0
Rohit <rohitsharma.rs4518@gmail.com>
2023-01-22 14:06:15 +0530

    New Pattern program Added

--- first_dart_app/bin/pattern/pattern_15.dart
+++ first_dart_app/bin/pattern/pattern_15.dart
@@ -6,25 +6,26 @@ void main(List<String> args) {
   //  *   *
   //   * *
   //    *
+  //   * *
+  //  *   *
+  // *     *
   int count = 5;
   int count_incre = 1;
-  // for (int i = 0; i <= 3; i++) {
-  //   stdout.write(" " * i);
-  //   stdout.write("*");
-  //   stdout.write(" " * count);
-  //   if (count < 1) {
-  //     break;
-  //   }
-  //   stdout.write("*");
-  //   stdout.write("\n");
-  //   count -= 2;
-  // }
-  for (int j = 0; j <= 3; j++) {
+  for (int i = 0; i <= 3; i++) {
+    stdout.write(" " * i);
+    stdout.write("*");
+    stdout.write(" " * count);
+    if (count < 1) {
+      break;
+    }
+    stdout.write("*");
+    stdout.write("\n");
+    count -= 2;
+  }
+  stdout.write("\n");
+  for (int j = 2; j >= 0; j--) {
     stdout.write(" " * j);
     stdout.write("*");
-    if (count_incre == 1) {
-      continue;
-    }
     stdout.write(" " * count_incre);
     stdout.write("*");
     stdout.write("\n");
--- first_dart_app/bin/pattern/pattern_18.dart
+++ first_dart_app/bin/pattern/pattern_18.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 
 void main(List<String> args) {
+  // WAP to print the following Pattern
+  // 1
+  // 121
+  // 12321
+  // 1234321
   for (int i = 1; i <= 4; i++) {
     for (int j = 1; j <= i; j++) {
       stdout.write(j);
-      if (i > 1) {
-        for (int k = i; k >= 1; k--) {
-          stdout.write(k);
-        }
-      } else {
-        break;
+    }
+    if (i > 1) {
+      for (int k = i - 1; k >= 1; k--) {
+        stdout.write(k);
       }
     }
     stdout.write("\n");
